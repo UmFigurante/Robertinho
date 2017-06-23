ffa7489b9aed329e7420ad674b1a00aa8b357fb8
UmFigurante <carloskill000@gmail.com>
2017-06-22 22:02:04 -0300

    Biblioteca BufferedStack
    Será nescessária para o futuro Robertinho

---
+++ Bibliotecas nescessárias/ActionBuffer/include/ActionBuffer.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "ActionBuffer.h"
+
+
+void BufferedStack::addToStack(StackableFunction func, void* arg)
+{
+	functions.push_back({ func, arg, false });
+}
+
+void BufferedStack::executeStack()
+{
+	for (StackMember sm : functions)
+		if (!sm.used)
+		{
+			sm.func((void*)sm.arg);
+			sm.used = true;
+		}
+			
+}
+
---
+++ Bibliotecas nescessárias/ActionBuffer/include/ActionBuffer.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+#include <thread>
+
+typedef void(*StackableFunction)(void* arg);
+typedef struct { StackableFunction func; void* arg; bool used; } StackMember;
+
+class BufferedStack
+{
+private:
+
+	int delayPerExecution;
+	std::vector<StackMember> functions;
+
+public:
+
+	
+	void addToStack(StackableFunction func, void* arg);
+
+	void executeStack();
+
+};
+
+
